bedb0a7fbc4cfa84fe39085573c76b5ad3d9c51d
Nathan Du <nathandu@outlook.com>
2025-02-14 14:29:37 +0800

    add tests for note inspection

--- tests/core/core.c
+++ tests/core/core.c
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: BSD-2-Clause
 #include <check.h>
 
-const char suite_name[] = "libtaiko";
+const char suite_name[] = "libtaco";
 
 extern TCase *case_course();
+extern TCase *case_note();
 extern TCase *case_section();
 
 TCase *(*const cases[])(void) = {
     case_course,
+    case_note,
     case_section,
     NULL,
 };
