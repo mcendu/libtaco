0f1d7e4cf91ec7a466fbf7f860a4b6bd6fabc0b3
Du Yijie <nathandu@outlook.com>
2025-07-22 16:04:29 +0800

    tja: remove tests of parser subcomponents
    Attempts to test these easily gets out of hand.

--- tests/tja/tja.c
+++ tests/tja/tja.c
@@ -3,13 +3,9 @@
 
 const char suite_name[] = "libtaco_tja";
 
-extern TCase *case_events();
 extern TCase *case_parser();
-extern TCase *case_segment();
 
 TCase *(*const cases[])(void) = {
-    case_events,
     case_parser,
-    case_segment,
     NULL,
 };
